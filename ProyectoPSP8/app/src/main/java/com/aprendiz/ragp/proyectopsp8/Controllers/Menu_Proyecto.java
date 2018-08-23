21121690b69aea624b72da1090423ea49e9124f3
DavidPyn27 <popayan159@gmail.com>
2018-08-23 16:08:36 -0500

    timerlog

--- ProyectoPSP8/app/src/main/java/com/aprendiz/ragp/proyectopsp8/Controllers/Menu_Proyecto.java
+++ ProyectoPSP8/app/src/main/java/com/aprendiz/ragp/proyectopsp8/Controllers/Menu_Proyecto.java
@@ -1,15 +1,70 @@
 package com.aprendiz.ragp.proyectopsp8.Controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.aprendiz.ragp.proyectopsp8.R;
 
-public class Menu_Proyecto extends AppCompatActivity {
+public class Menu_Proyecto extends AppCompatActivity implements View.OnClickListener{
+
+
+    Button btnTimer, btnDefect, btnProyectPlan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu__proyecto);
+
+
+        inicializar();
+        escuchar();
+    }
+
+    private void escuchar() {
+        btnTimer.setOnClickListener(this);
+        btnDefect.setOnClickListener(this);
+        btnProyectPlan.setOnClickListener(this);
+
+    }
+
+    private void inicializar() {
+
+        btnTimer = findViewById(R.id.btnTimerLog);
+        btnDefect = findViewById(R.id.btnDefectLog);
+        btnProyectPlan = findViewById(R.id.btnProyectPlanSummary);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.btnTimerLog:
+
+                Intent intent = new Intent(Menu_Proyecto.this, TimerLog.class);
+                startActivity(intent);
+
+                break;
+
+
+            case R.id.btnDefectLog:
+                Intent intent1 = new Intent(Menu_Proyecto.this, DefectLog.class);
+                startActivity(intent1);
+
+                break;
+
+
+            case R.id.btnProyectPlanSummary:
+
+                Intent intent2 = new Intent(Menu_Proyecto.this, ProyectPlanSummary.class);
+                startActivity(intent2);
+
+                break;
+        }
+
     }
 }
--- ProyectoPSP8/app/src/main/java/com/aprendiz/ragp/proyectopsp8/Controllers/TimerLog.java
+++ ProyectoPSP8/app/src/main/java/com/aprendiz/ragp/proyectopsp8/Controllers/TimerLog.java
@@ -5,11 +5,20 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.aprendiz.ragp.proyectopsp8.R;
 
-public class TimerLog extends AppCompatActivity {
+public class TimerLog extends AppCompatActivity implements View.OnClickListener {
+
+    Spinner spinnerPhas;
+    EditText txtStart, txtInterupcion, txtStop, txtDelta, txtComentarios;
+    Button btnStart, btnStop;
+
 
     private TextView mTextMessage;
 
@@ -38,9 +47,35 @@ public class TimerLog extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer_log);
 
+
+        inicializar();
+        //escuchar();
+
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    private void inicializar() {
+
+        spinnerPhas = findViewById(R.id.SpinnerPhase);
+
+        txtStart = findViewById(R.id.txtStart);
+        txtStop = findViewById(R.id.txtStop);
+        txtInterupcion = findViewById(R.id.txtInterrupciones);
+        txtDelta = findViewById(R.id.txtDelta);
+        txtComentarios = findViewById(R.id.txtComentarios);
+
+        btnStart = findViewById(R.id.btnStart);
+        btnStop = findViewById(R.id.btnStop);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
